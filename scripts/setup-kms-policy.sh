da1f90999e3f6df408cae15bc83eb84859ad73b3
0xktn <0xktn@users.noreply.github.com>
2025-12-09 16:02:36 +0700

    fix: update default iam role name to EnclaveInstanceRole

--- scripts/setup-kms-policy.sh
+++ scripts/setup-kms-policy.sh
@@ -22,7 +22,7 @@ state_set "pcr0" "$PCR0" --encrypt
 AWS_REGION=$(state_get "aws_region" || echo "ap-southeast-1")
 AWS_ACCOUNT_ID=$(state_get "aws_account_id" || aws sts get-caller-identity --query Account --output text)
 KEY_ID=$(state_get "kms_key_id" || echo "901ee892-db48-4a51-903a-25d46a721c8e")
-ROLE_NAME=$(state_get "iam_role_name" || echo "enclave-role")
+ROLE_NAME=$(state_get "iam_role_name" || echo "EnclaveInstanceRole")
 
 echo "Applying attestation policy..."
 echo "PCR0: $PCR0"
